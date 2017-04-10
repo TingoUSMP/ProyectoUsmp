f870d89dde0b1636d9e3717845fd421d7a4fb544
unknown <ronald_tingo@usmp.pe>
2017-04-10 12:27:44 +0500

    segundo commit de prueba

---
+++ Consola1/Consola1/Radio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola1
+{
+    public class Radio
+    {
+        public string Name{ get; set; }
+    }
+}
